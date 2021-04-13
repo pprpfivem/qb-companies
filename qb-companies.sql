3880f8735272419298ff2e17a43909a35d42f7b7
GhzGarage <qbcore@programmer.net>
2021-04-13 16:01:36 -0500

    sql

--- qb-companies.sql
+++ qb-companies.sql
@@ -1,6 +1,16 @@
+-- Not Sure Which One Yet
+
 CREATE TABLE `companies` (
   `name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
   `label` varchar(50) COLLATE utf8mb4_bin NOT NULL,
   `investRate` float DEFAULT NULL,
   `rate` varchar(10) COLLATE utf8mb4_bin NOT NULL DEFAULT 'stale'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `companies` (
+  `name` tinytext DEFAULT NULL,
+  `label` tinytext DEFAULT NULL,
+  `owner` tinytext DEFAULT NULL,
+  `employees` tinytext DEFAULT NULL,
+  `money` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
