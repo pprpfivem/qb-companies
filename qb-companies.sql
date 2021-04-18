8a3d5ec4ad5b8cfb31a58b5be12aa7df6b2a6826
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-04-18 02:01:03 +0200

    Removed wrong table

--- qb-companies.sql
+++ qb-companies.sql
@@ -1,12 +1,3 @@
--- Not Sure Which One Yet
-
-CREATE TABLE `companies` (
-  `name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
-  `label` varchar(50) COLLATE utf8mb4_bin NOT NULL,
-  `investRate` float DEFAULT NULL,
-  `rate` varchar(10) COLLATE utf8mb4_bin NOT NULL DEFAULT 'stale'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
 CREATE TABLE `companies` (
   `name` tinytext DEFAULT NULL,
   `label` tinytext DEFAULT NULL,
